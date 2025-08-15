1975240258f601a4fb17380d6f7c5732f3883b80
Mark de Heij <m.deheij@vdlp.nl>
2025-08-15 11:09:03 +0200

    Remove Pathhelper

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -9,7 +9,6 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\File;
 use Laravel\Telescope\Telescope;
 use RuntimeException;
-use Vdlp\Telescope\Classes\PathHelper;
 
 final class HomeController extends Controller
 {
@@ -20,23 +19,6 @@ final class HomeController extends Controller
     {
         return view('vdlp.telescope::layout', [
             'cssFile' => BrandSetting::getColorMode() === BrandSetting::COLOR_DARK ? 'app-dark.css' : 'app.css',
-            'telescopeScriptVariables' => Telescope::scriptVariables(),
-            'assetsAreCurrent' => $this->assetsAreCurrent(),
         ]);
-    }
-
-    private function assetsAreCurrent(): bool
-    {
-        $pathHelper = new PathHelper();
-
-        $publishedPath = $pathHelper->getAssetsPath('mix-manifest.json');
-
-        if (!File::exists($publishedPath)) {
-            throw new RuntimeException('The Telescope assets are not published. Please run: php artisan telescope:publish');
-        }
-
-        $vendorPath = base_path('vendor/laravel/telescope/public/mix-manifest.json');
-
-        return File::get($publishedPath) === File::get($vendorPath);
     }
 }
