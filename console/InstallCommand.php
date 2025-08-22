963aa194df67dac6e1ef19a3c9e0e3c7c67f8719
Mark de Heij <m.deheij@vdlp.nl>
2025-08-22 08:18:47 +0200

    Extend the telescope install command

--- console/InstallCommand.php
+++ console/InstallCommand.php
@@ -4,21 +4,13 @@ declare(strict_types=1);
 
 namespace Vdlp\Telescope\Console;
 
-use Illuminate\Console\Command;
+use Laravel\Telescope\Console\InstallCommand as TelescopeInstallCommand;
 
-final class InstallCommand extends Command
+final class InstallCommand extends TelescopeInstallCommand
 {
-    public function __construct()
-    {
-        $this->signature = 'telescope:install';
-        $this->description = 'Install all of the Telescope resources';
-
-        parent::__construct();
-    }
-
     public function handle(): void
     {
-        $this->comment('Publishing Telescope migrations...');
+        $this->comment('Publishing Telescope Migrations...');
         $this->callSilent('vendor:publish', ['--tag' => 'telescope-migrations']);
 
         $this->comment('Publishing Telescope Configuration...');
